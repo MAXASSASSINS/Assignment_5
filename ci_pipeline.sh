8e00d9ccae8d736a7c17e32afeb1a3a5e1efc713
MAXASSASSINS <mohammedshadab23april@gmail.com>
2025-07-15 12:32:16 +0530

    v3 -> v4

--- ci_pipeline.sh
+++ ci_pipeline.sh
@@ -6,15 +6,15 @@ sudo cp -r "/mnt/c/Users/mohd.c.shadab/OneDrive - Accenture/Documents/Desktop/Do
 
 cd assignment_5
 
-docker build -t my-producer:v3 -f Dockerfile.producer .
-docker build -t my-consumer:v3 -f Dockerfile.consumer .
-docker build -t my-postgres:v3 -f Dockerfile.postgres .
+docker build -t my-producer:v4 -f Dockerfile.producer .
+docker build -t my-consumer:v4 -f Dockerfile.consumer .
+docker build -t my-postgres:v4 -f Dockerfile.postgres .
 
-docker save my-consumer:v3 > ~/my-consumer.tar
+docker save my-consumer:v4 > ~/my-consumer.tar
 microk8s ctr image import ~/my-consumer.tar
 
-docker save my-producer:v3 > ~/my-producer.tar
+docker save my-producer:v4 > ~/my-producer.tar
 microk8s ctr image import ~/my-producer.tar
 
-docker save my-postgres:v3 > ~/my-postgres.tar
+docker save my-postgres:v4 > ~/my-postgres.tar
 microk8s ctr image import ~/my-postgres.tar
